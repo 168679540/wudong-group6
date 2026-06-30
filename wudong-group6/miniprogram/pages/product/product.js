00de60ab6cce584f407bb5edbb7814e322bcf615
林泽铃 <168679540@qq.com>
2026-06-30 20:54:19 +0800

    feat: 小程序商品页增加大图+详情弹窗+规格购买完整流程

--- wudong-group6/miniprogram/pages/product/product.js
+++ wudong-group6/miniprogram/pages/product/product.js
@@ -1,12 +1,15 @@
 var api = require('../../utils/api');
-Page({ data: { list: [], cats: ['全部'], cat: '', minPrice: '', maxPrice: '', buyTarget: null, specQty: 1, specSize: '', specColor: '' },
+Page({ data: { list: [], cats: ['全部'], cat: '', minPrice: '', maxPrice: '', detail: null, buyTarget: null, specQty: 1, specSize: '', specColor: '' },
   onLoad() { api.getCategories().then(r => r.success && this.setData({ cats: ['全部', ...r.data.map(c => c.name)] })); this.load(); },
   load() { var p = { pageSize: 50 }; if (this.data.cat) p.category = this.data.cat; if (this.data.minPrice) p.minPrice = Number(this.data.minPrice); if (this.data.maxPrice) p.maxPrice = Number(this.data.maxPrice); api.getProducts(p).then(r => r.success && this.setData({ list: r.data || [] })); },
   setCat(e) { var c = e.currentTarget.dataset.c; this.setData({ cat: c === '全部' ? '' : c }); this.load(); },
   onMin(e) { this.setData({ minPrice: e.detail.value }); this.load(); },
   onMax(e) { this.setData({ maxPrice: e.detail.value }); this.load(); },
-  // 选规格购买
-  openBuy(e) { var id = e.currentTarget.dataset.id; var p = this.data.list.find(x => x.id === id); if (!p) return; var specs = {}; try { if (p.specs) specs = typeof p.specs === 'string' ? JSON.parse(p.specs) : p.specs; } catch(_){} var sizes = specs['尺寸'] || specs['size'] || []; var colors = specs['颜色'] || specs['color'] || []; this.setData({ buyTarget: p, specQty: 1, specSize: sizes[0] || '', specColor: colors[0] || '' }); },
+  openDetail(e) { var id = e.currentTarget.dataset.id; var p = this.data.list.find(x => x.id === id); if (p) this.setData({ detail: p }); },
+  closeDetail() { this.setData({ detail: null }); },
+  buyFromDetail() { var p = this.data.detail; this.setData({ detail: null }); if (p) this.openBuyById(p.id); },
+  openBuyById(id) { var p = this.data.list.find(x => x.id === id); if (!p) return; var specs = {}; try { if (p.specs) specs = typeof p.specs === 'string' ? JSON.parse(p.specs) : p.specs; } catch(_){} var sizes = specs['尺寸'] || specs['size'] || []; var colors = specs['颜色'] || specs['color'] || []; this.setData({ buyTarget: p, specQty: 1, specSize: sizes[0] || '', specColor: colors[0] || '' }); },
+  openBuy(e) { this.openBuyById(e.currentTarget.dataset.id); },
   closeBuy() { this.setData({ buyTarget: null }); },
   onSpecSize(e) { this.setData({ specSize: e.detail.value }); },
   onSpecColor(e) { this.setData({ specColor: e.detail.value }); },
