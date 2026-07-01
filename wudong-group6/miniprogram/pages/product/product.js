d0c0cf183e05fbd5171f826fa82187e1bd7778f8
林泽铃 <168679540@qq.com>
2026-07-01 12:21:11 +0800

    fix: 首页热门推荐点击后重试查找直到加载完成打开详情

--- wudong-group6/miniprogram/pages/product/product.js
+++ wudong-group6/miniprogram/pages/product/product.js
@@ -2,18 +2,17 @@ var IMG = 'http://127.0.0.1:3000';
 function fixImg(url) { if (!url) return ''; return url.startsWith('/') ? IMG + url : url; }
 var api = require('../../utils/api');
 Page({ data: { list: [], cats: ['全部'], cat: '', minPrice: '', maxPrice: '', detail: null, buyTarget: null, specQty: 1, specSizes: [], specColors: [], specSizeIdx: 0, specColorIdx: 0 },
-  onLoad() { api.getCategories().then(r => r.success && this.setData({ cats: ['全部', ...r.data.map(c => c.name)] })); this.load(); },
-  onShow() { var id = wx.getStorageSync('wudong_goto_product'); if (id) { wx.removeStorageSync('wudong_goto_product'); var that = this; setTimeout(function() { that.openDetail({ currentTarget: { dataset: { id: id } } }); }, 300); } },
-  load() { var p = { pageSize: 50 }; if (this.data.cat) p.category = this.data.cat; if (this.data.minPrice) p.minPrice = Number(this.data.minPrice); if (this.data.maxPrice) p.maxPrice = Number(this.data.maxPrice); api.getProducts(p).then(r => { if (r.success) { var list = (r.data || []).map(function(item) { item.coverImage = fixImg(item.coverImage); return item; }); this.setData({ list: list }); } }); },
+  onLoad() { var that = this; api.getCategories().then(function(r) { if (r.success) that.setData({ cats: ['全部', ...r.data.map(function(c) { return c.name; })] }); }); this.load(); },
+  onShow() { var id = wx.getStorageSync('wudong_goto_product'); if (id) { wx.removeStorageSync('wudong_goto_product'); var that = this; this.tryOpenProduct(Number(id), 0); } },
+  tryOpenProduct(id, retry) { var that = this; var p = this.data.list.find(function(x) { return x.id == id; }); if (p) { that.openDetail({ currentTarget: { dataset: { id: id } } }); } else if (retry < 15) { setTimeout(function() { that.tryOpenProduct(id, retry + 1); }, 300); } },
+  load() { var p = { pageSize: 50 }; if (this.data.cat) p.category = this.data.cat; if (this.data.minPrice) p.minPrice = Number(this.data.minPrice); if (this.data.maxPrice) p.maxPrice = Number(this.data.maxPrice); var that = this; api.getProducts(p).then(function(r) { if (r.success) { var list = (r.data || []).map(function(item) { item.coverImage = fixImg(item.coverImage); return item; }); that.setData({ list: list }); } }); },
   setCat(e) { var c = e.currentTarget.dataset.c; this.setData({ cat: c === '全部' ? '' : c }); this.load(); },
   onMin(e) { this.setData({ minPrice: e.detail.value }); this.load(); },
   onMax(e) { this.setData({ maxPrice: e.detail.value }); this.load(); },
-  openDetail(e) { var id = e.currentTarget.dataset.id; var p = this.data.list.find(x => x.id == id); if (p) { var d = Object.assign({}, p); d.coverImage = fixImg(d.coverImage); this.setData({ detail: d }); } },
-  noop() {},
-  addToCart(e) { var p = this.data.list.find(function(x) { return x.id == e.target.dataset.id; }); if (!p) { p = this.data.detail; if (!p) return; } var cart = wx.getStorageSync('wudong_cart') || []; var uid = 'p' + p.id; var exist = cart.find(function(x) { return x.uid === uid; }); if (exist) { exist.quantity = (exist.quantity || 1) + 1; } else { cart.push({ uid: uid, productId: p.id, name: p.name, image: p.coverImage, price: Number(p.price), quantity: 1, merchantId: p.merchantId }); } wx.setStorageSync('wudong_cart', cart); wx.showToast({ title: '已加入购物车' }); },
+  openDetail(e) { var id = e.currentTarget.dataset.id; var p = this.data.list.find(function(x) { return x.id == id; }); if (p) { var d = Object.assign({}, p); d.coverImage = fixImg(d.coverImage); this.setData({ detail: d }); } },
   closeDetail() { this.setData({ detail: null }); },
   buyFromDetail() { var p = this.data.detail; this.setData({ detail: null }); if (p) this.openBuyById(p.id); },
-  openBuyById(id) { var p = this.data.list.find(x => x.id == id); if (!p) return;
+  openBuyById(id) { var p = this.data.list.find(function(x) { return x.id == id; }); if (!p) return;
     var specs = {}; try { if (p.specs) specs = typeof p.specs === 'string' ? JSON.parse(p.specs) : p.specs; } catch(_){}
     var sizes = specs['尺寸'] || specs['size'] || [];
     var colors = specs['颜色'] || specs['color'] || [];
@@ -21,15 +20,17 @@ Page({ data: { list: [], cats: ['全部'], cat: '', minPrice: '', maxPrice: '', 
     this.setData({ buyTarget: bt, specQty: 1, specSizes: sizes, specColors: colors, specSizeIdx: 0, specColorIdx: 0 });
   },
   openBuy(e) { this.openBuyById(e.currentTarget.dataset.id); },
+  noop() {},
   closeBuy() { this.setData({ buyTarget: null }); },
+  addToCart(e) { var p = this.data.list.find(function(x) { return x.id == e.target.dataset.id; }); if (!p) { p = this.data.detail; if (!p) return; } var cart = wx.getStorageSync('wudong_cart') || []; var uid = 'p' + p.id; var exist = cart.find(function(x) { return x.uid === uid; }); if (exist) { exist.quantity = (exist.quantity || 1) + 1; } else { cart.push({ uid: uid, productId: p.id, name: p.name, image: p.coverImage, price: Number(p.price), quantity: 1, merchantId: p.merchantId }); } wx.setStorageSync('wudong_cart', cart); wx.showToast({ title: '已加入购物车' }); },
   onSizeChange(e) { this.setData({ specSizeIdx: Number(e.detail.value) }); },
   onColorChange(e) { this.setData({ specColorIdx: Number(e.detail.value) }); },
   onQty(e) { this.setData({ specQty: Number(e.detail.value) || 1 }); },
   doBuy() { var p = this.data.buyTarget; if (!p) return;
     var size = this.data.specSizes[this.data.specSizeIdx] || '';
     var color = this.data.specColors[this.data.specColorIdx] || '';
-    var specDesc = [size, color].filter(Boolean).join(' ');
-    var name = p.name; if (specDesc) name += ' ' + specDesc; name += ' ×' + this.data.specQty;
-    api.createOrder({ type: '商品', amount: Number(p.price) * this.data.specQty, merchantId: p.merchantId, itemName: name, itemImage: p.coverImage }).then(r2 => { if (r2.success) { wx.showToast({ title: '购买成功！' }); this.setData({ buyTarget: null }); } else { wx.showToast({ title: r2.message || '下单失败', icon: 'none' }); } }).catch(() => wx.showToast({ title: '下单失败', icon: 'none' }));
+    var specDesc = [size, color].filter(Boolean).join(' '); var name = p.name; if (specDesc) name += ' ' + specDesc; name += ' ×' + this.data.specQty;
+    var that = this;
+    api.createOrder({ type: '商品', amount: Number(p.price) * this.data.specQty, merchantId: p.merchantId, itemName: name, itemImage: p.coverImage }).then(function(r2) { if (r2.success) { wx.showToast({ title: '购买成功！' }); that.setData({ buyTarget: null }); } else { wx.showToast({ title: r2.message || '下单失败', icon: 'none' }); } }).catch(function() { wx.showToast({ title: '下单失败', icon: 'none' }); });
   }
 });
